e642d48747f475c56e88affa72850ee2b34b9c0b
Viktor <viktor.hidirov@yandex.ru>
2022-10-17 09:05:10 +0700

    fix

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -3,12 +3,12 @@ const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 const popupCloseButtonElement = popupElement.querySelector('.popup__button-close');
 
 const openPopup = function (event) {
-  popupElement.classList.add('popup__open');
+  popupElement.classList.add('popup_opened');
   nameInput.value = userName.textContent;
   jobInput.value = userProfission.textContent;
 }
 const closePopup = function (event) {
-  popupElement.classList.remove('popup__open');
+  popupElement.classList.remove('popup_opened');
 }
 const closePopupOnClickOverlay = function (event) {
   if (event.target !== event.currentTarget) {
