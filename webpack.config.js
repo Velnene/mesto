57fa70ece986ce682cd1118231b8bd3cca09eeee
Viktor <viktor.hidirov@yandex.ru>
2023-01-02 22:49:50 +0700

    feat: перенес файлы в src

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
 
 module.exports = {
-  entry: { main: './scripts/index.js' }, //указали первое место, куда заглянет webpack, — файл index.js в папке src
+  entry: { main: './src/scripts/index.js' }, //указали первое место, куда заглянет webpack, — файл index.js в папке src
   output: {
     path: path.resolve (__dirname, 'dist'),
     filename: 'main.js',
@@ -48,7 +48,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-    template: './index.html' // путь к файлу index.html
+    template: './src/index.html' // путь к файлу index.html
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(), // подключение плагина для объединения файлов
